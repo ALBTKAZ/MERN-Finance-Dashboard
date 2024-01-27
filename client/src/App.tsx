275d53c9c45099c23ddcc853909cf2d5b3845e5b
TKAZ <albertliu321@gmail.com>
2024-01-27 01:50:04 -0800

    Testing CssBaseLine msg in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> 
+        hi
+      </ThemeProvider>
     </div>
   )
 }
 
-export default App
+export default App;
