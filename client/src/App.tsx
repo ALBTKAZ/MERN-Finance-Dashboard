017e67f339f99e4d7bcbcc9335448ef6e396a63c
TKAZ <albertliu321@gmail.com>
2024-01-27 23:22:07 -0800

    Material UI setting for "Box"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="app">
+      <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline /> 
-        hi
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+        </Box>
       </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
